dd1a057dc91ecfcf3e2891441c8160fe93fed0dc
delwiv <delwiv@protonmail.com>
2024-02-14 16:49:24 +0100

    feat: add documentation

--- lua/lspconfig/server_configurations/tabby.lua
+++ lua/lspconfig/server_configurations/tabby.lua
@@ -3,6 +3,25 @@ local util = require 'lspconfig.util'
 return {
   default_config = {
     cmd = { 'npx', 'tabby-agent', '--lsp', '--stdio' },
-    filetypes = { '*' },
+    filetypes = {
+      '*',
+    },
+    root_dir = util.root_pattern('package.json', 'requirements.txt', '.git', 'Makefile'),
+  },
+  docs = {
+    description = [[
+https://tabby.tabbyml.com/blog/running-tabby-as-a-language-server
+
+Running tabby as a language server
+
+CoC example adapted to nvim-lspconfig
+
+```lua
+require'lspconfig'.tabby.setup()
+```
+    ]],
+    default_config = {
+      root_dir = [[root_pattern("package.json", "requirements.txt", ".git", "Makefile")]],
+    },
   },
 }
